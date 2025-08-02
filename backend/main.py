443266b2e1523f892f26e5e774eb8d19cc7acc32
Sruthi <sruthyy345@gmail.com>
2025-08-02 06:05:35 +0530

    no port

--- backend/main.py
+++ backend/main.py
@@ -198,6 +198,6 @@ def generate_and_save_audio( text, output_wav_path='output.wav', voice_name='Kor
     print(f"Audio saved to {output_wav_path}")
 
 generate_and_save_audio("കണക്കിൽ ഇത്രയും കുറഞ്ഞ മാർക്കോ? നിന്നെ ഞാൻ എന്ത് ചെയ്യാനാ!", voice_name='schedar')
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="localhost", port=80)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="localhost", port=80)
